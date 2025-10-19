e5532257a82095a67ef8d7212db52715e8a973a3
Neha-Patil-12 <106377584+Neha-Patil-12@users.noreply.github.com>
2025-10-19 12:37:52 +0530

    Frontend Initialization + Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="text-3xl font-bold">
+        <h1>Welcome to RoadSense.ai</h1>
+      </div>   
+    </>
+  )
+}
+
+export default App
